b52259326b984af6f71469a3024c574d277773c6
Chaffre <t.chaffre@gmail.com>
2017-03-31 11:08:18 +0200

    Interface player

--- src/main/java/GLPOO_ESIEA_1617/Groupe_Velay/Player.java
+++ src/main/java/GLPOO_ESIEA_1617/Groupe_Velay/Player.java
@@ -1,5 +1,28 @@
 package GLPOO_ESIEA_1617.Groupe_Velay;
 
 public interface Player {
-
+	
+	int score = 0;
+	int posX = 0;
+	int posY = 0;
+	
+	
+	public void movePlayer(char dir);
+	public void moveTop();
+	public void moveDown();
+	public void moveRight();
+	public void moveLeft();
+	public void pickUp();
+	public void AddScore();
+	
+	public void setScore();
+	public void setPosX();
+	public void setPosY();
+	
+	public int getScore();
+	public int getPosX();
+	public int getPosY();
+	
+	
+	
 }
