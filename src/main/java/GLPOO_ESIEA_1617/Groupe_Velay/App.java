9d97ae9fee623c643b8801b9daf4c09f8fc027fa
Marc-Velay <marc.velay@gmail.com>
2017-03-31 09:13:53 +0200

    init commit

---
+++ src/main/java/GLPOO_ESIEA_1617/Groupe_Velay/App.java
@@ -0,0 +1,13 @@
+package GLPOO_ESIEA_1617.Groupe_Velay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
