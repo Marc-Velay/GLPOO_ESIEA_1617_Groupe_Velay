77eaaa4eac02690b89166e9517c92328533bd9fa
Marc-Velay <marc.velay@gmail.com>
2017-03-31 09:52:20 +0200

    Interfaces created.

--- src/main/java/GLPOO_ESIEA_1617/Groupe_Velay/Egg.java
+++ src/main/java/GLPOO_ESIEA_1617/Groupe_Velay/Egg.java
@@ -1,5 +1,7 @@
 package GLPOO_ESIEA_1617.Groupe_Velay;
 
 public interface Egg {
-
+	int posX =0;
+	int posY =0;
+	
 }
--- src/main/java/GLPOO_ESIEA_1617/Groupe_Velay/Rock.java
+++ src/main/java/GLPOO_ESIEA_1617/Groupe_Velay/Rock.java
@@ -1,5 +1,6 @@
 package GLPOO_ESIEA_1617.Groupe_Velay;
 
 public interface Rock {
-
+	int posX =0;
+	int posY =0;
 }
