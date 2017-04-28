ca0c9566a6c0db58eddc018ad08b64f81e28bd63
Alistair <bakeralistair95@gmail.com>
2017-04-28 16:30:39 +0200

    Added default tests.

---
+++ src/test/java/glpoo_esiea_1617_velay/KidTest.java
@@ -0,0 +1,26 @@
+package glpoo_esiea_1617_velay;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class KidTest extends TestCase {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail();
+	}
+
+}
---
+++ src/test/java/glpoo_esiea_1617_velay/MenuTest.java
@@ -0,0 +1,24 @@
+package glpoo_esiea_1617_velay;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MenuTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
